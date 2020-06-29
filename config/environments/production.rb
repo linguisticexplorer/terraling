7d704cb37bb4083255b668a9f5a6d0539b9761f8
Shailesh Vasandani <github@shaile.sh>
2020-06-28 20:17:24 -0400

    Add SMTP information from environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,8 @@ LinguisticExplorer::Application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'terraling.com',
-    user_name:            '',
-    password:             '',
+    user_name:            ENV["SMTP_USER"],
+    password:             ENV["SMTP_PASS"],
     authentication:       :plain,
     enable_starttls_auto: true
   }
