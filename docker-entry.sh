da1ec0d1a565c44ac9129fbceebdebbb2d9c7aa1
Shailesh Vasandani <github@shaile.sh>
2020-07-10 12:43:25 -0400

    Temporary rspec configuration commit

--- docker-entry.sh
+++ docker-entry.sh
@@ -2,12 +2,16 @@
 
 export RECAPTCHA_SITE_KEY=6LcYxasZAAAAAL25mMYFfzO-DmDEHjqkFVw0d7_5
 export RECAPTCHA_SECRET_KEY=6LcYxasZAAAAAM3ZSKVicnCsKx4U5D262UzdAUEP
+export DATABASE_CLEANER_ALLOW_PRODUCTION=true
+
+while true; do foo; sleep 10; done
+# RAILS_ENV=production LOCAL_DEV=true bundle exec rspec
 
 # compile static assets
-bundle exec rake assets:clobber && bundle exec rake assets:precompile
+# bundle exec rake assets:clobber && bundle exec rake assets:precompile
 
 # remove the server pid
-rm -f tmp/pids/server.pid
+# rm -f tmp/pids/server.pid
 
 # start the server
-RAILS_ENV=production LOCAL_DEV=true bundle exec rails s -b 0.0.0.0
+# RAILS_ENV=production LOCAL_DEV=true bundle exec rails s -b 0.0.0.0
