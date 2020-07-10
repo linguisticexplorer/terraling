f496f445a435108a6a6726d1b81043fd925a5d23
Shailesh Vasandani <github@shaile.sh>
2020-07-09 21:47:44 -0400

    Add localhost CAPTCHA env variables to docker script

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export RECAPTCHA_SITE_KEY=6LcYxasZAAAAAL25mMYFfzO-DmDEHjqkFVw0d7_5
+export RECAPTCHA_SECRET_KEY=6LcYxasZAAAAAM3ZSKVicnCsKx4U5D262UzdAUEP
+
 # compile static assets
 bundle exec rake assets:clobber && bundle exec rake assets:precompile
 
