5da800e44e4949252db63074515c816a4f9bf1b3
Shailesh Vasandani <github@shaile.sh>
2020-07-11 23:51:28 -0400

    Clarify purpose of RSpec testing docker-compose

--- docker-entry.rspec.sh
+++ docker-entry.rspec.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export RECAPTCHA_SITE_KEY=6LcYxasZAAAAAL25mMYFfzO-DmDEHjqkFVw0d7_5
-export RECAPTCHA_SECRET_KEY=6LcYxasZAAAAAM3ZSKVicnCsKx4U5D262UzdAUEP
-export DATABASE_CLEANER_ALLOW_PRODUCTION=true
+echo 'export DATABASE_CLEANER_ALLOW_PRODUCTION=true' >> ~/.bashrc 
 
 while true; do foo; sleep 10; done
 # RAILS_ENV=production LOCAL_DEV=true bundle exec rspec
