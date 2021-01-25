8142edcf6fda2ee54130deaa36bc92ca544d9dc8
hab278 <hab278@nyu.edu>
2021-01-24 22:24:12 -0500

    Use env vars for deploy port

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $(dirname $0)
-RAILS_ENV=production bundle exec puma -t 1:4 -w 1 --preload -p 4000 --pidfile tmp/pids/puma.pid --state tmp/puma.state --control-url tcp://127.0.0.1:9293 --control-token foo 2> log/puma-production-4000.error > log/puma-production-4000.log &
+RAILS_ENV=production bundle exec puma -t 1:4 -w 1 --preload -p $DEPLOY_PORT --pidfile tmp/pids/puma.pid --state tmp/puma.state --control-url tcp://127.0.0.1:9293 --control-token foo 2> log/puma-production-$DEPLOY_PORT.error > log/puma-production-$DEPLOY_PORT.log &
