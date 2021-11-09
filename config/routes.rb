9ce403bbaf24b18f23cadf05d894934e17d0dd55
svasandani <vasandanishailesh@gmail.com>
2021-11-09 11:51:31 -0500

    Revert "Integrate new edit-values page"
    This reverts commit 57582aad71dab1889eabab7eaaf80aab5ae4f000.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 LinguisticExplorer::Application.routes.draw do
 
+  get 'hello_world', to: 'hello_world#index'
   devise_for  :users, :controllers => { :registrations => "users/registrations" }
 
   root        :to => 'home#index'
